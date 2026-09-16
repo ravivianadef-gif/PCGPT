460157b8907d8c4cf8af0beb0374a3631529cc9b
ravivianadef-gif <ravivianadef@gmail.com>
2026-09-16 18:41:27 -0300

    chore: bump service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pcgpt-v11';
+const CACHE='pcgpt-v12';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./engine.js','./operator.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
